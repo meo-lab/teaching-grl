5ec199b71a1e1f878feed34813b2e749c15943ce
Marc Rußwurm <marc.russwurm@wur.nl>
2026-04-27 19:25:58 +0200

    initial commit

---
+++ layouts/bonn-content.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import logoUrl from '../assets/uni-bonn-logo.png'
+</script>
+
+<template>
+  <div class="slidev-layout bonn-layout bonn-content-layout">
+    <main class="bonn-content">
+      <slot />
+    </main>
+    <img class="bonn-logo" :src="logoUrl" alt="University of Bonn" />
+  </div>
+</template>
---
+++ layouts/bonn-cover.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import logoUrl from '../assets/uni-bonn-logo.png'
+
+const props = defineProps<{
+  background?: string
+  subhead?: string
+}>()
+
+const style = computed(() => {
+  if (!props.background)
+    return {}
+
+  return {
+    backgroundImage: `linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.18) 100%), url("${props.background}")`,
+  }
+})
+</script>
+
+<template>
+  <div class="slidev-layout bonn-layout bonn-cover-layout" :style="style">
+    <div class="bonn-cover-panel">
+      <p v-if="subhead" class="bonn-cover-subhead">{{ subhead }}</p>
+      <slot />
+    </div>
+    <img class="bonn-logo" :src="logoUrl" alt="University of Bonn" />
+  </div>
+</template>
